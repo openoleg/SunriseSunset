73fe78d7a91f0ae3ba7a7ec8e6ffe8d1b110b244
Oleg Voznyuk <dev.openoleg@gmail.com>
2018-07-21 08:36:39 +0300

    Create empty DaylightActivity

---
+++ app/src/main/java/com/openoleg/sunrisesunset/presentation/daylight/DaylightActivity.java
@@ -0,0 +1,15 @@
+package com.openoleg.sunrisesunset.presentation.daylight;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.openoleg.sunrisesunset.R;
+
+public class DaylightActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daylight);
+    }
+}
